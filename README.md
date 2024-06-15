1f6f6b32238d5172f2ce70acb50252b444f6dbae
Fabio Tetsuo <fabioctetsuo@gmail.com>
2024-06-15 00:09:09 -0300

    aula 08 - tipos de dados primitivos

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #### aula 07
 
 - Criação de variáveis e constantes
+
+#### aula 08
+
+- Explicação sobre tipos de dados primitivos
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// tipos de dados primitivos numeros
 let height = 168;
 let weight = 65;
-
 const conversionFactor = 0.01; // declarar quando explicar que precisa transformar altura de cm para metros
 
 height *= conversionFactor;
 
 const imc = weight / (height * height);
 
-console.log(imc);
+// tipo de dado primitivo string
+const result = "O resultado do IMC é de " + imc;
+
+// tipo de dado booleano
+const isHealth = true;
+
+// tipo de dado nulo
+const isNull = null;
+
+console.log(result);
